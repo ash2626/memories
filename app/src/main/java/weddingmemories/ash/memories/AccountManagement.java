b718cb1ab674e343ad30850e26d90cda77e0b374
John Ashton <ash2626@hotmail.com>
2016-10-02 22:06:49 +0100

    Adding account creation

--- app/src/main/java/weddingmemories/ash/memories/AccountManagement.java
+++ app/src/main/java/weddingmemories/ash/memories/AccountManagement.java
@@ -1,12 +1,7 @@
 package weddingmemories.ash.memories;
 
-import android.accounts.Account;
-import android.content.AbstractThreadedSyncAdapter;
-import android.content.ContentProviderClient;
-import android.content.Context;
-import android.content.SyncResult;
 import android.os.AsyncTask;
-import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -16,6 +11,8 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.HttpURLConnection;
 
+import static weddingmemories.ash.memories.R.string.username;
+
 /**
  * Created by joannewebster3 on 02/10/2016.
  */
@@ -24,28 +21,26 @@ public class AccountManagement extends AsyncTask {
 
 
     public AccountManagement() {
+
     }
 
     @Override
     protected Object doInBackground(Object[] objects) {
-        String dataUrl = "http://ash2626:Sycam0re@192.168.1.6/ocs/v1.php/cloud/users -d";
-        String dataUrlParameters = "userid=" + "frank" + " -d &password=" + "frankspassword";
+        String dataUrl = "http://192.168.1.6/owncloud/ocs/v1.php/cloud/users";
+        String dataUrlParameters = "-d userid=\"Frank\" -d password=\"frankspassword\"";
+        String basicAuth = "Basic " + Base64.encodeToString("ash2626:Sycam0re".getBytes(), Base64.URL_SAFE|Base64.NO_WRAP);
         URL url;
         HttpURLConnection connection = null;
         try {
 // Create connection
             url = new URL(dataUrl);
             connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("Authorization", basicAuth);
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setRequestProperty("Content-Length", "" + Integer.toString(dataUrlParameters.getBytes().length));
-            connection.setRequestProperty("Content-Language", "en-US");
-            connection.setUseCaches(false);
             connection.setDoInput(true);
             connection.setDoOutput(true);
 // Send request
-            DataOutputStream wr = new DataOutputStream(
-                    connection.getOutputStream());
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
             wr.writeBytes(dataUrlParameters);
             wr.flush();
             wr.close();
